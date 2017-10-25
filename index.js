55012e086720ff0007fedb95eec8e00ad5d6eced
Kevin Chen <1909916440@qq.com>
2017-10-25 12:01:33 +0800

    webpack and babel config

--- index.js
+++ index.js
@@ -1,33 +1,116 @@
-const stackCopy = function (obj){
-	let _this= this;
-	let result = null;
-	function isArray (ele){
-		if(Object.prototype.toString.call(ele) === '[object Array]'){
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var stackCopy = function stackCopy(obj) {
+	var _this2 = this;
+
+	var _this = this;
+	var result = null;
+	function isArray(ele) {
+		if (Object.prototype.toString.call(ele) === '[object Array]') {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
-	if(isArray(obj)){
+	if (isArray(obj)) {
 		result = [];
-		obj.map((ele, i) => {
-			if(isArray(obj[i]) || (typeof obj[i] === 'object' && (obj[i] !== null))){
-				result[i] = this.$copy(obj[i]);
-			}else{
-				result[i] = obj[i];	
+		obj.map(function (ele, i) {
+			if (isArray(obj[i]) || _typeof(obj[i]) === 'object' && obj[i] !== null) {
+				result[i] = _this2.$copy(obj[i]);
+			} else {
+				result[i] = obj[i];
 			}
 		});
-	}else if(typeof obj === 'object' && obj !== null){
+	} else if ((typeof obj === 'undefined' ? 'undefined' : _typeof(obj)) === 'object' && obj !== null) {
 		result = {};
-		for(let o in obj){
-			if(obj.hasOwnProperty(o)){
+		for (var o in obj) {
+			if (obj.hasOwnProperty(o)) {
 				result[o] = this.$copy(obj[o]);
 			}
 		}
-	}else{
+	} else {
 		result = obj;
 	}
 	return result;
-}
+};
 
-export default stackCopy;
+exports.default = stackCopy;
+
+/***/ })
+/******/ ]);
